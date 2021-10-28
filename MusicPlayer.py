efa0f81e3bf12c490af633cdd7ed8d4b2976651f
DonutDev <58142952+DonutDev@users.noreply.github.com>
2021-10-28 14:57:44 +0300

    Updated lastest version. CHOOSE BUTTON DOES NOT WORK

--- MusicPlayer.py
+++ MusicPlayer.py
@@ -1,4 +1,4 @@
-#variables and importing stuff here 
+#variables and importing stuff here
 import pygame
 from pygame import mixer
 from tkinter import *
@@ -27,10 +27,46 @@ def stopsong():
 
 def resumesong():
     songstatus.set("Resuming")
-    mixer.music.unpause() 
+    mixer.music.unpause()
 def loopsong():
     songstatus.set("Looping")
-    mixer.music.play(loops=-1) 
+    mixer.music.play(loops=-1)
+def volup():
+	mixer.music.set_volume(min(1.0,mixer.music.get_volume()+0.1))
+	#mixer.music.play()
+def voldown():
+	mixer.music.set_volume(max(0.0,mixer.music.get_volume()-0.1))
+	#mixer.music.play()
+def playAll():
+    print("Playing file:")
+    playlist_pos=0
+    for file in playlist.get(0,END):
+    	print("Playing file:")
+    	if file.endswith('.mp3'):
+    		print("Playing file:", file)
+    		playlist.selection_clear(0,END)
+    		playlist.selection_set(first=playlist_pos)
+    		playlist.activate(playlist_pos)
+    		mixer.music.load(file)
+    		mixer.music.play()
+    		playing_state = True
+		    # Wait for the music to play before exiting
+    		playlist_pos=playlist_pos+1
+    		if playlist_pos>playlist.size()-1:
+		       playlist_pos=0
+    		while mixer.music.get_busy():
+		       pygame.time.Clock().tick(500)
+
+def loadfolder():
+    path = askdirectory(title='Select Music Folder')
+    self.dirs = os.listdir(path)
+    self.lst_pos = self.lst.size()
+    for file in self.dirs:
+    	if file:
+    		if file.endswith('.mp3'):
+	        	self.lst.insert(self.lst_pos,path + '/' + file)
+	        	self.musicdirs.append(path + '/' + file)
+	        	self.lst_pos=self.lst_pos+1
 
 root=Tk()
 root.title('PySong')
@@ -41,7 +77,7 @@ songstatus.set("choosing")
 
 #GUI---------------
 
-playlist=Listbox(root,selectmode=SINGLE,bg="DodgerBlue2",fg="white",font=('arial',15),width=40)
+playlist=Listbox(root,selectmode=SINGLE,bg="#282828",fg="white",font=('arial',15),width=40)
 playlist.grid(columnspan=5)
 
 os.chdir(path)
@@ -51,23 +87,39 @@ for s in songs:
 
 
 playbtn=Button(root,text="Play",command=playsong)
-playbtn.config(font=('Ubuntu',20),bg="DodgerBlue2",fg="white",padx=7,pady=7)
+playbtn.config(font=('Ubuntu',20),bg="grey",fg="white",padx=7,pady=7)
 playbtn.grid(row=1,column=0)
 
+playbtn=Button(root,text="Play All",command=playAll)
+playbtn.config(font=('Ubuntu',20),bg="grey",fg="white",padx=7,pady=7)
+playbtn.grid(row=1,column=1)
+
 pausebtn=Button(root,text="Pause",command=pausesong)
-pausebtn.config(font=('Ubuntu',20),bg="DodgerBlue2",fg="white",padx=7,pady=7)
-pausebtn.grid(row=1,column=1)
+pausebtn.config(font=('Ubuntu',20),bg="grey",fg="white",padx=7,pady=7)
+pausebtn.grid(row=1,column=2)
 
 stopbtn=Button(root,text="Stop",command=stopsong)
-stopbtn.config(font=('Ubuntu',20),bg="DodgerBlue2",fg="white",padx=7,pady=7)
-stopbtn.grid(row=1,column=2)
+stopbtn.config(font=('Ubuntu',20),bg="grey",fg="white",padx=40,pady=7)
+stopbtn.grid(row=1,column=3)
 
 Resumebtn=Button(root,text="Resume",command=resumesong)
-Resumebtn.config(font=('Ubuntu',20),bg="DodgerBlue2",fg="white",padx=7,pady=7)
-Resumebtn.grid(row=1,column=3)
+Resumebtn.config(font=('Ubuntu',20),bg="grey",fg="white",padx=7,pady=7)
+Resumebtn.grid(row=1,column=4)
 
 Loopbtn=Button(root,text="Loop",command=loopsong)
-Loopbtn.config(font=('Ubuntu',20),bg="DodgerBlue2",fg="white",padx=7,pady=7)
-Loopbtn.grid(row=1,column=4)
+Loopbtn.config(font=('Ubuntu',20),bg="grey",fg="white",padx=25,pady=7)
+Loopbtn.grid(row=2,column=4)
+
+VolUp = Button(root ,text = '+',  width = 3, font = ('Times', 10), command=volup)
+VolUp.config(font=('Ubuntu',20),bg="grey",fg="white",padx=1,pady=1)
+VolUp.grid(row=2,column=0)
+
+VolDown = Button(root ,text = '-',  width = 3, font = ('Times', 10), command=voldown)
+VolDown.config(font=('Ubuntu',20),bg="grey",fg="white",padx=1,pady=1)
+VolDown.grid(row=2,column=1)
+
+Addbtn=Button(root,text="Add Music",command=loadfolder)
+Addbtn.config(font=('Ubuntu',20),bg="grey",fg="white",padx=7,pady=7)
+Addbtn.grid(row=2,column=3)
 
 mainloop()
